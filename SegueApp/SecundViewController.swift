612fca72128e3a905f465cc548a1029e2efdbd3e
MapKit <porfiriomendoza2509@gamil.com>
2024-08-29 10:51:12 -0600

    first commit

---
+++ SegueApp/SecundViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecundViewController.swift
+//  SegueApp
+//
+//  Created by Jazmin on 08/08/24.
+//
+
+import UIKit
+
+class SecundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
